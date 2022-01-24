f90be2c22777008b3da1e1939253cb54b86ebc84
Fatima Uzair <fatimauzair888@gmail.com>
2022-01-24 06:53:02 -0800

    Update

--- index.mjs
+++ index.mjs
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('im express.js server')
 })
 
+app.get('/about', (req, res) => {
+  res.send('This is something about server')
+})
+
+
 app.listen(port, () => {
   console.log(`Hello server is listening at  ${port}`)
 })
